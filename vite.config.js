33278b4b9fc7f92e6aec284082c67bcc07ee4570
Dream OS Developer <dreamos.sch.id@gmail.com>
2026-07-14 13:22:25 +0700

    Fix MIME type error - relative asset paths

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   server: {
     port: 5173,
     strictPort: false
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    assetsDir: 'assets'
   }
 })
